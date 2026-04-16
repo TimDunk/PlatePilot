23604e911dd9ebb96497e571bd9c867a59c4562d
Duncan <1902886398@qq.com>
2026-04-16 23:50:15 +0100

    1. fix bug 2. Add all vendors's cart 3.add favorite vendors list page

--- js/CartItem.js
+++ js/CartItem.js
@@ -13,9 +13,9 @@ class CartItem{
 
 class CartItemModel{
     constructor(){
+        this.maxCartId=0;
         this.cartArr=[];
         this.loadData();  //TODO:delete this line
-        this.maxCartId=0;
         this.toppingModel=this.getToppingModel();
         this.variantModel=this.getVariantModel();
     }
@@ -37,8 +37,8 @@ class CartItemModel{
                 this.cartArr=parsedArray.map(data => new CartItem(data));
             this.cartArr.forEach(
                 cart => {
-                    if(cart.id > this.maxCartId)
-                        this.maxCartId=cart.id;
+                    if(cart.cartId > this.maxCartId)
+                        this.maxCartId=cart.cartId;
                 }
             );
         } catch (error) {
@@ -268,9 +268,18 @@ class CartItemModel{
                 let newCart=this.newCart(vendor,item,quantity,selectedVariants,SelectedToppings,instruction);
                 this.setFee(newCart,vendor);
                 this.cartArr.push(newCart);
+                this.maxCartId=this.maxCartId+1;
             }
         }
         sessionStorage.setItem("CartData",JSON.stringify(this.cartArr));
+    }
+
+    removeCart(cartId){
+        const cartIndex=this.cartArr.findIndex(i => i.cartId==cartId);
+        if( cartIndex!=undefined && cartIndex>=0){
+            this.cartArr.splice(cartIndex,1); 
+            sessionStorage.setItem("CartData",JSON.stringify(this.cartArr));
+        }
     }
 
 }
